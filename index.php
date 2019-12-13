146045b3dd5c5dfb991e37d77c0db1388424e857
Munir <munir@anodyne.dk>
2019-12-13 15:03:54 +0100

    initial commit of the whole project

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
-<?php include "includes/functions.php" ?>
-<?php include "includes/header.php" ?>
-<?php include "includes/slider.php" ?>
-<?php include "includes/categories.php" ?>
-<?php include "includes/gallery.php" ?>
-<?php include "includes/blog.php" ?>
-<?php include "includes/footer.php" ?>
+<?php get_header(); ?>
+<section class="banner">
+            <div class="banner-image" style="background-image: url(<?php echo get_theme_file_uri("assets/images/herobw.jpg")?>);"></div>
+</section>
+    <section class="content">
+            <div class="container">
+
+
+            <?php 
+            while(have_posts()){
+                the_post(); ?>
+            <div class="post-item">
+                <h2><a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+            </div>
+
+            <?php }
+            ?>
+            </div>
+    </section>
+
+
+<?php get_footer(); ?>
